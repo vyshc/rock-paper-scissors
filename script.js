63aff33fd0fab5676b3c42018b80ef47027dff56
vysh <work.vyshc@gmail.com>
2024-07-29 09:35:39 +0530

    refactor and clean up code

--- script.js
+++ script.js
@@ -1,68 +1,74 @@
+//functionality: 
+//user clicks on button to choose --> userChoice
+//computer generates the choice already --> computerChoice
+//compare the choices
+//declare the winner for the round --> create a div element 
+//put winner in the div element --> update the scores after every round 
+//-->change the innerText and set it to the score variables --> userScore and computerSccore
+//when either of the score becomes 5 --> add a new paragraph to the div element -->declare the final winner
+//--> keep running scores as is and add the final winner
+//--> then reset the div element --> remove the element 
+//have a button to refresh the page? //optional
+
+//first generate computerChoice using getComputerChoice
+const body = document.querySelector("body");
+const div = document.createElement("div");
 const buttons = document.querySelectorAll("button");
 
-buttons.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    let userScore      = 0,
-        computerScore  = 0,
-        computerChoice,
-        userChoice     = e.target.id;
-    const res   = document.createElement("div");
-    const body  = document.querySelector("body");
-    res.innerText = "";
+body.appendChild(div);
 
-    function playRound() {
-      function getComputerChoice() {
-        const rndm = Math.floor(Math.random() * 3) + 1;
-        switch (rndm) {
-          case 1: {
-            res.innerText += "\nComputer chose Rock";
-            computerChoice = "rock";
-            break;
-          }
-          case 2: {
-            res.innerText += "\nComputer chose Paper";
-            computerChoice = "paper";
-            break;
-          }
-          case 3: {
-            res.innerText += "\nComputer chose Scissors";
-            computerChoice = "scissors";
-            break;
-          }
-        }
-      }
-      getComputerChoice();
-      res.innerText += `\nYou chose ${userChoice}`;
+let computerChoice = "", userChoice ="", computerScore = 0, userScore = 0;
 
-      if (computerChoice == userChoice) {
-        res.innerText += "tie!";
-      } else if (
-        (computerChoice == "paper" && userChoice == "scissors") ||
-        (computerChoice == "scissors" && userChoice == "rock") ||
-        (computerChoice == "rock" && userChoice == "paper")
-      ) {
-        res.innerText += "you win!";
-        userScore++;
-      } else {
-        res.innerText += "computer wins!";
-        computerScore++;
-      }
-
-      res.innerText += `\n Running score: \n Computer - ${computerScore} \n You - ${userScore}`;
-
-      body.appendChild(res);
+function getComputerChoice() {
+  const rndm = Math.floor(Math.random() * 3) + 1;
+  switch (rndm) {
+    case 1: {
+      return "rock";
     }
+    case 2: {
+      return "paper";
+    }
+    case 3: {
+      return "scissors";
+    }
+  }
+}
 
-    playRound();
+//define computerScore and userScore
 
-    //Display the running score, and announce a winner of the game once one player reaches 5 points. 
+function compareChoice(uc){
+  computerChoice = getComputerChoice();
+  console.log("computer chose:", computerChoice);
 
-    // if(userScore == 5 ){
-    //     res.innerText += "You are the WInner of this round"
-    //     return;
-    // }else if (computerScore == 5){
-    //     res.innerText += "Computer is the WInner of this round"
-    //     return;
-    // }
-  });
-});
+  if (
+    (computerChoice == "paper" && userChoice == "scissors") ||
+    (computerChoice == "scissors" && userChoice == "rock") ||
+    (computerChoice == "rock" && userChoice == "paper")
+  ) {
+    userScore++;
+  } else if (uc == computerChoice) {
+    console.log("tie - no change in the scores");
+  } else{
+    computerScore++;
+  }
+}
+
+function playRound(){
+  let runningScore =  `\n running score: ` + `\n your score: ${userScore}` + `\n computer score: ${computerScore}`;
+  div.innerText = runningScore;
+  console.log(computerScore, userScore);
+
+  if(userScore==5 || computerScore == 5){
+    (userScore > computerScore) ? div.innerText = "You win!!!"   : div.innerText = "computer wins!!!";
+  }
+}
+
+//get userChoice from the buttons and play!
+
+buttons.forEach(button => button.addEventListener("click", ()=>{
+  div.innerText = "";
+  console.log(`you chose: ${button.id}`);
+  userChoice = button.id;
+  compareChoice(userChoice);
+  playRound();
+})); 
